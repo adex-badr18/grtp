dc7329fd1a1f5476ecec1eda9517bb69f65923d0
Badrudeen Adewumi Abdul-hameed <adex.badr18@gmail.com>
2023-12-27 11:45:49 +0100

    refactor: Create contact form

--- src/components/subscribe/Subscribe.jsx
+++ src/components/subscribe/Subscribe.jsx
@@ -1,5 +1,5 @@
 import './Subscribe.css';
-import {TiSocialGooglePlus} from 'react-icons/ti';
+import { TiSocialGooglePlus } from 'react-icons/ti';
 import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 import { useEffect } from "react";
@@ -17,12 +17,33 @@ const Subscribe = () => {
     return (
         <section id='subscribe'>
             <div className="container subscribe" data-aos='zoom-in-left'>
-                <h2>Subscribe Now!</h2>
+                <div className="u-title">
+                    <h2>Contact Us</h2>
+                    <p className="u-text-small">Have questions or need assistance? Our dedicated team is here to help. Fill the form below to send us a message.</p>
+                </div>
                 <form action="">
-                    <div className="form-control">
-                        <input type="text" placeholder='Enter Your Email...' />
-                        <button>Subscribe</button>
+                    <div className="form-row">
+                        <div className="form-input">
+                            <label htmlFor="fullName">Full Name</label>
+                            <input type="text" id='fullName' name='fullName' placeholder='Full Name' />
+                        </div>
+                        <div className="form-input">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id='email' name='email' placeholder='Email' />
+                        </div>
                     </div>
+
+                    <div className="form-input">
+                        <label htmlFor="subject">Subject</label>
+                        <input type="text" id='subject' name='subject' placeholder='Subject' />
+                    </div>
+
+                    <div className="form-input">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="5" placeholder='Compose your message...'></textarea>
+                    </div>
+
+                    <button type='submit'>Submit</button>
                 </form>
 
                 <div className="social-icons">
